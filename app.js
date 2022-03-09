f7cfb740ee1671cc8f5d9228bbed5845ebb2feb2
RenijS <renijshrestha8@gmail.com>
2022-03-09 15:28:15 +1100

    add

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const addBtn = document.querySelector(".addBtn");
 const title = document.querySelector("#title");
 const pages = document.querySelector("#pages");
 const author = document.querySelector("#author");
+const haveRead = document.querySelector("#haveRead");
+const bookInput = document.querySelectorAll(".bookInput");
 
 let book = function (title, author, pages, haveRead) {
   this.title = title;
@@ -25,24 +27,26 @@ bookLibrary.push(
 
 function addBookToLibrary(book) {
   bookLibrary.push(book);
-
+  displayAllBooks(bookLibrary);
+  /*
   for (const items in book) {
     const card = document.createElement("div");
     card.classList.add("card");
-    /*
+    
     const info = document.createElement("p");
     info.value = `book.${items}`;
     console.log(info.value);
     card.appendChild(info);
     displaySection.appendChild(card);
-    */
     displaySection.value = `book.${items}`;
     displaySection.appendChild(card);
   }
   console.log(bookLibrary);
+  */
 }
 
 function displayAllBooks(bookLibrary) {
+  displaySection.innerHTML = "";
   if (bookLibrary.length != 0) {
     for (let i = 0; i < bookLibrary.length; i++) {
       let book = bookLibrary[i];
@@ -64,7 +68,26 @@ function displayAllBooks(bookLibrary) {
 displayAllBooks(bookLibrary);
 
 addBtn.addEventListener("click", function () {
-  addBookToLibrary(
-    new book(title.value, author.value, pages.value, haveRead.value)
-  );
+  if (title.value == "") {
+    title.placeholder = "Please enter title";
+    title.classList.add("error");
+  } else if (author.value == "") {
+    author.placeholder = "Please enter author name";
+    author.classList.add("error");
+  } else if (pages.value == "") {
+    pages.placeholder = "Please enter a value";
+    pages.classList.add("error");
+  } else {
+    addBookToLibrary(
+      new book(title.value, author.value, pages.value, haveRead.value)
+    );
+  }
+});
+
+bookInput.forEach((element) => {
+  element.addEventListener("input", () => {
+    if (element.classList.contains("error")) {
+      element.classList.remove("error");
+    }
+  });
 });
